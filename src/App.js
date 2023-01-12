56c6fb0c0a910a575d11bd48e7b57632f44d7d44
Kudosans <yoyoyo20201@gmail.com>
2023-01-12 21:52:04 +0900

    first-commit16

--- src/App.js
+++ src/App.js
@@ -7,27 +7,14 @@ import { Amplify } from 'aws-amplify';
 //import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "./ui-components";
+import { NavBar, studioTheme } from "./ui-components";
 
 //Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
   );
 }
